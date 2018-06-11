d79ac421cd2d9d9f77973adad7f1ff23a60f6d2a
Le Tien Tai <letientai299@gmail.com>
2018-06-11 14:56:54 +0800

    Add reason for skipping mathematical problems
    TLDR: I'm lazy to type down the math.

--- chapter-01/1.12.recursive.rkt
+++ chapter-01/1.12.recursive.rkt
@@ -2,15 +2,15 @@
 
 ; Sample:
 ; 1
-; 1 1 
+; 1 1
 ; 1 2 1
 ; 1 3 3 1
-; The value is in 2d space, thus, we will need 2 indexes to refer to a value. 
-; We can define a function f(x, y), such that x is a non negative integer, and 
+; The value is in 2d space, thus, we will need 2 indexes to refer to a value.
+; We can define a function f(x, y), such that x is a non negative integer, and
 ; y in a non negative integer that lesser than or equal to x+1.
-; So: 
+; So:
 ; - f(x, y) = 1 if y = 0 or y = x+1 (the edges)
-; - f(x, y) = f(x-1, y) + f(x-1, y-1) 
+; - f(x, y) = f(x-1, y) + f(x-1, y-1)
 
 (define (f x y)
   (cond ((= y 0) 1)
@@ -19,7 +19,7 @@
 
 ; Try it:
 (f 0 0) ; 1
-(f 1 0) ; 1 
+(f 1 0) ; 1
 (f 1 1) ; 1
 (f 3 1) ; 3
 (f 4 2) ; 6
---
+++ readme.md
@@ -0,0 +1,14 @@
+Solution as code for SICP
+=========================
+
+The book: http://sarabander.github.io/sicp/
+
+The code is written using `#lang racket/base`. I actually don't know much about
+Racket (and Scheme), just learn enough to walk thought he book. I only implement
+the exercises that I found interesting and won't take so much time. For purely
+mathematical problems (like 1.13), I won't type down the solution (too lazy,
+also, vimwiki and markdown don't support Math well).
+
+Study note is written using [vimwiki](https://github.com/vimwiki/vimwiki),
+mostly random though, open questions and aha moments. It won't be helpful if you
+plan to get knowledge from the book without read it.
