0c0645b1089fc95d326130233f853b39af0ece64
Le Tien Tai <letientai299@gmail.com>
2018-06-11 14:21:38 +0800

    Add some answers for chapter 1

---
+++ chapter-01/fib.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
+(fib 7)
+(fib 8)
+(fib 9)
+(fib 10)
