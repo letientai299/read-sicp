cfebffd3e27ac76962725e1868f2a96286639df3
Tai <letientai299@gmail.com>
2024-06-28 10:22:58 +0700

    Done ex 1.9 and 1.10

---
+++ ch01/1.10.ackermann.rkt
@@ -0,0 +1,48 @@
+#lang debug racket
+
+(define (A x y)
+  (cond
+    [(= y 0) 0]
+    [(= x 0) (* 2 y)]
+    [(= y 1) 2]
+    [else (A (- x 1) (A x (- y 1)))]))
+
+; define the h(n) function to test our custom implementation.
+(define (h n)
+  (A 2 n))
+
+; A special https://en.wikipedia.org/wiki/Tetration function
+; that returns 0 if n=0 instead of 1.
+(define (tetration n)
+  (cond
+    [(>= 0 n) 0]
+    [(= 1 n) 2]
+    [else (power-of-2 (tetration (- n 1)))]))
+
+; custom power of 2 in a tail-recursive manner,
+; instead of using expt function, just for fun.
+(define (power-of-2 n)
+  (define (square x)
+    (* x x))
+  (define (iter base rest more)
+    (if (>= 1 rest)
+        (* base more)
+        (iter (square base)
+              (quotient rest 2)
+              (* more (if (odd? rest) base 1)))))
+  (if (>= 0 n) 1 (iter 2 n 1)))
+
+(for ([i (in-inclusive-range 0 20)])
+  (printf "~a -> ~a\n" i (power-of-2 i)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;                                    TEST                                   ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(module+ test
+  (require rackunit)
+  ; can't test with n>=6 since the computation is too slow.
+  (for ([n (in-inclusive-range 1 5)])
+    (define result-h (h n))
+    (define result-tetration (tetration n))
+    (test-case (format "h(~a)" n)
+      (check-eqv? result-h result-tetration))))
---
+++ ch01/factorials.rkt
@@ -0,0 +1,47 @@
+#lang debug racket
+
+(define (fact-iter n)
+  (define (iter prod count)
+    (if (> count n)
+        prod
+        (iter ;
+         (* prod count)
+         (+ count 1))))
+  (iter 1 1))
+
+(define (fact-recursive n)
+  (if (>= 0 n) ;
+      1
+      (* n (fact-recursive (- n 1)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;                                   TEST                                    ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(module+ test
+  (require rackunit)
+
+  (define cases
+    (list
+     (cons 0 1)
+     (cons 1 1)
+     (cons 3 6)
+     (cons 5 120)
+     (cons 10 3628800)
+     (cons 12 479001600)
+     (cons 30 265252859812191058636308480000000)
+     (cons 40
+           815915283247897734345611269596115894272000000000)
+     (cons
+      52
+      80658175170943878571660636856403766975289505440883277824000000000000)))
+
+  (define (test-fn case)
+    (define n (car case))
+    (define want (cdr case))
+    (test-case (format "iter: ~a -> ~a" n want)
+      (check-eqv? want (fact-iter n)))
+    (test-case (format "recursive:  ~a -> ~a" n want)
+      (check-eqv? want (fact-recursive n))))
+
+  (for-each test-fn cases))
